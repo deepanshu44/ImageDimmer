b73d4f3746d04e4169d74581041b932c70314096
deepanshu <62979448+deepanshu44@users.noreply.github.com>
2025-01-13 01:01:58 +0530

    Add video and SVG tag support

--- content_script.js
+++ content_script.js
@@ -1,14 +1,30 @@
 let styleEl = document.createElement('style');
 document.body.appendChild(styleEl);
 
-browser.storage.onChanged.addListener(function (changes, area) {
+browser.storage.onChanged.addListener(async function (changes, area) {
     if (area === 'local' && 'image_dimmer_value' in changes) {
-	update(changes.image_dimmer_value.newValue)
+	updateCss(changes.image_dimmer_value.newValue)
     }
 });
 
-function update(value) {
-    styleEl.innerText = `img { opacity: ${value} !important; }`;
+function updateCss(value) {
+    styleEl.innerText = `img { opacity: ${value.slider_val} !important; }`;
+    if (value.video) {
+	styleEl.innerText = `${styleEl.innerText} video { opacity: ${value.slider_val} !important; }`;
+    }
+    if (value.svg) {
+	styleEl.innerText = `${styleEl.innerText} svg { opacity: ${value.slider_val} !important; }`;
+    }
 }
 
-browser.storage.local.get(['image_dimmer_value']).then(result => update(result.value));
+browser.storage.local.get('image_dimmer_value')
+    .then(result => {
+	if (result.slider_val) {
+	    updateCss(result.image_dimmer_value)
+	} else throw "empty object"
+    })
+    .catch(async () => {
+	// First time installed. Update storage
+	// default opacity value set to 0.8
+	await browser.storage.local.set({ image_dimmer_value:{slider_val:0.8,video:true,svg:true} })
+    });
--- popup.js
+++ popup.js
@@ -1,18 +1,36 @@
-let valueEl = document.querySelector('.image_dimmer_slider');
+let valueEl = document.getElementById('image_dimmer_slider');
+let video = document.getElementById("image_dimmer_video_opt");
+let svg = document.getElementById("image_dimmer_svg_opt");
 
+//init
 async function init() {
-    let { image_dimmer_value } = await browser.storage.local.get(['image_dimmer_value']);
-    if (!image_dimmer_value) {
-	image_dimmer_value = 0;
+    let {image_dimmer_value} = await browser.storage.local.get("image_dimmer_value")
+    valueEl.value = image_dimmer_value.slider_val
+    video.checked = image_dimmer_value.video
+    svg.checked = image_dimmer_value.svg
+}
+
+init().catch(() => {})
+
+async function setValue(element) {
+    const {id} = element
+    let updated_value = {};
+    let {image_dimmer_value} = await browser.storage.local.get("image_dimmer_value");
+    if (id === "image_dimmer_slider") {
+	updated_value = {...image_dimmer_value, slider_val : element.value};
+	
+    } else if (id === "image_dimmer_video_opt") {
+	updated_value = {...image_dimmer_value, video : element.checked};
+	
+    } else if (id === "image_dimmer_svg_opt") {
+	updated_value = {...image_dimmer_value, svg : element.checked};
+	
     }
-    setValue(image_dimmer_value);
+    await browser.storage.local.set({ image_dimmer_value:updated_value });
+    // valueEl.image_dimmer_value = updated_value;
 }
 
-async function setValue(image_dimmer_value) {
-    await browser.storage.local.set({ image_dimmer_value });
-    valueEl.image_dimmer_value = image_dimmer_value;
-}
-
-valueEl.addEventListener('change', e => setValue(e.target.value));
-
-init().catch(e => console.error(e));
+// modify the listeners below so that "this" value can be accessed
+valueEl.addEventListener('change', e => setValue(e.target));
+video.addEventListener('change', e => setValue(e.target));
+svg.addEventListener('change', e => setValue(e.target));
